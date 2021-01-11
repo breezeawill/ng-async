c20bd2ee11e06d36c4dbd49c38c4ff727783d801
Anthony Williams <anthony.williams@flybreeze.com>
2021-01-11 11:32:08 -0700

    Add first example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { interval, Observable, Observer } from 'rxjs'
+import { take } from 'rxjs/operators'
 
 @Component({
   selector: 'app-root',
+  styleUrls: ['./app.component.sass'],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'ng-async';
+  title = 'ng-async'
+
+  executeFirstExample(): void {
+    // stream of events / changes / pieces of data
+    const observable: Observable<number> = interval(1000).pipe(take(10))
+
+    // consumer of stream
+    const observer: Observer<number> = {
+      complete: () => {
+        console.log('Complete!')
+      },
+      error: (err) => {
+        console.error(err)
+      },
+      next: (val) => {
+        console.log(val)
+      },
+    }
+
+    // begin consumer listening
+    observable.subscribe(observer)
+  }
 }
